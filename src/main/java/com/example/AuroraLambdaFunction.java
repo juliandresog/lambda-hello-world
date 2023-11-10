c1a269e5b0cac582c3489df98604d0fe017a647a
Julian Osorio <juliandresog@yahoo.com>
2023-11-10 18:22:19 -0500

    Pruebas llamando funcion sql

--- src/main/java/com/example/AuroraLambdaFunction.java
+++ src/main/java/com/example/AuroraLambdaFunction.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-/*
+ /*
  * AuroraLambdaFunction Versión 1.0 7 nov 2023
  *
  * Copyright(c) 2007-2023, Boos IT.
@@ -10,11 +10,11 @@
  *
  * http://talento.cloud/license
  **/
-
 package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -23,7 +23,8 @@ import java.sql.SQLException;
 
 /**
  * La descripcion de la clase va aqui.
- * @version 	1.0 7 nov 2023
+ *
+ * @version 1.0 7 nov 2023
  * @author josorio
  */
 public class AuroraLambdaFunction implements RequestHandler<Object, String> {
@@ -36,31 +37,72 @@ public class AuroraLambdaFunction implements RequestHandler<Object, String> {
     public String handleRequest(Object input, Context context) {
         Connection connection = null;
         try {
-            System.out.println("path: "+AURORA_DB_URL);
+            System.out.println("path: " + AURORA_DB_URL);
             // Establish the database connection
             connection = DriverManager.getConnection(AURORA_DB_URL, AURORA_DB_USERNAME, AURORA_DB_PASSWORD);
 
-            // Execute SQL query
-            String sql = "select codigo, descripcion from public.trv_propiedades limit 3";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet resultSet = statement.executeQuery();
+            String callSQL = "{ call public.trv_funcion_descartable(?, ?) }";
+            try (CallableStatement pstmt = connection.prepareCall(callSQL)) {
+                
+                pstmt.setInt(1, 200);
+                pstmt.setInt(2, 2013);
 
-            // Process the results if needed
-            StringBuilder ultimo = new StringBuilder();
-            while (resultSet.next()) {
-                // Handle each row of the result set
-                System.out.println("code: "+resultSet.getString(1));
-                System.out.println("description: "+resultSet.getString(2));
-                ultimo.append(resultSet.getString(2)).append("\n");
+                boolean currentResult = pstmt.execute();
+                while (true) {
+                    if (currentResult) {
+                        try (ResultSet rs = pstmt.getResultSet()) {
+                            // process result set
+                        }
+                    } else {
+                        int updateCount = pstmt.getUpdateCount();
+                        if (updateCount == -1) {
+                            // no more results
+                            break;
+                        }
+                        // do something with update count
+                    }
+                    currentResult = pstmt.getMoreResults();
+                }
+
+            } catch (SQLException e) {
+                // Handle database errors
+                e.printStackTrace();
+                return "Error executing query: " + e.getMessage();
+            } finally {
+                // Close the database connection
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-            
-            //connection.close();
 
-            return "Query executed successfully! Encontrado:\n"+ultimo.toString();
+//            // Execute SQL query
+//            //String sql = "select codigo, descripcion from talento_demo.trv_propiedades limit 3";
+//            PreparedStatement statement = connection.prepareStatement(sql);
+//            ResultSet resultSet = statement.executeQuery();
+//
+//            // Process the results if needed
+//            StringBuilder ultimo = new StringBuilder();
+//            while (resultSet.next()) {
+////                // Handle each row of the result set
+////                System.out.println("code: "+resultSet.getString(1));
+////                System.out.println("description: "+resultSet.getString(2));
+////                ultimo.append(resultSet.getString(2)).append("\n");
+//            }
+
+            //connection.close();
+            return "Query executed successfully! Encontrado:\n";
         } catch (SQLException e) {
             // Handle database errors
             e.printStackTrace();
             return "Error executing query: " + e.getMessage();
+        } catch (Exception e) {
+            // Handle database errors
+            e.printStackTrace();
+            return "Error no controlado executing query: " + e.getMessage();    
         } finally {
             // Close the database connection
             if (connection != null) {
